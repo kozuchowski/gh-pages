37ed9974edf2d47684437c658c56cbb4c31ac0fa
Kozuchowski <marekkozuchowski@vp.pl>
2020-10-19 14:57:39 +0200

    Updates

---
+++ precache-manifest.b1a370e1571500e0de5047eddadaffdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d654bd86fc9fc17e4ef0ce387e4f9e8",
+    "url": "/gh-pages/index.html"
+  },
+  {
+    "revision": "82128e848688516291b4",
+    "url": "/gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1266935d95f08bec3b18",
+    "url": "/gh-pages/static/js/2.bde0b825.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/gh-pages/static/js/2.bde0b825.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82128e848688516291b4",
+    "url": "/gh-pages/static/js/main.3286341b.chunk.js"
+  },
+  {
+    "revision": "66cc75388667ad41ed35",
+    "url": "/gh-pages/static/js/runtime-main.c233f5c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
